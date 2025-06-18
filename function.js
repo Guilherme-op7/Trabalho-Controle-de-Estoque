5ff326576fc43b145dd5d9c1de6c45ec26c4683a
Ana Clara Rocha Dantas <ra49908285858@cursos.insf>
2025-06-18 14:52:11 -0300

    alteração ana

--- function.js
+++ function.js
@@ -40,3 +40,15 @@ export function apagarProduto(){
         console.log('#Produto invalido')
     }
 }
+
+
+//função de somar o total do estoque -- Ana Clara 
+ export function calcularValorEstoque (){ 
+    let valorTotal= 0; //Valor total começa com 0 
+     
+    for (let produtos of produtos){
+        valorTotal + produco.preco * produto.quantidade; //Calcular o valor total dos produtos e adiciona á valorTotal
+    }
+
+    console.log(`O valor total do estoque é: ${valorTotal}`)//Apresenta o valor total do estoque ao usuário
+ }
