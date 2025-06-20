9beb4f2441b91c6c79615642f8913b4d07a38fbd
guilherme-op7 <netoguilherme989@gmail.com>
2025-06-20 13:55:08 -0300

    ajustando o codigo

---
+++ app.js
@@ -0,0 +1,88 @@
+import prompt from 'prompt-sync'
+let ler = prompt();
+
+import { apagarProduto, calcularValorEstoque } from './function.js';
+
+
+
+//Função de Apresentar menu Lucas Gabriel
+
+async function MostraMenu () {
+
+    let funcao = 1;
+    while (funcao !== 0) {
+
+    
+
+    console.log("=== SISTEMA DE ESTOQUE 🎮 ===")
+    
+    await sleep(200)
+    console.log("0. Sair")
+    await sleep(200)
+    console.log("1. Cadastrar novo produto")
+    await sleep(200)
+    console.log("2. Listar todos os produtos")
+    await sleep(200)
+    console.log("3. Adicionar Quantidade ao estoque")
+    await sleep(200)
+    console.log("4. Retirar quantidade do estoque")
+    await sleep(200)
+    console.log("5. Apagar produto")
+    await sleep(200)
+    console.log("6. Calcular valor do Estoque")
+    await sleep(200)
+    console.log("7. Buscar produto pelo nome")
+    
+    await sleep(200)
+
+    console.log("Qual função você irá usar?")
+    funcao = Number(ler())
+    
+    if (funcao == 1) {
+        //função não foi criada ainda
+    }
+
+    else if (funcao == 2) {
+        //função não foi criada ainda
+    }
+
+    else if (funcao == 3) {
+        //função não foi criada ainda
+    }
+
+    else if (funcao == 4) {
+        //função não foi criada ainda
+    }
+
+    else if (funcao == 5) {
+       apagarProduto();
+    }
+
+    else if (funcao == 6) {
+        calcularValorEstoque();
+    }
+
+    else if (funcao == 7) {
+
+    }
+
+    else if (funcao == 0) {
+        console.log("Saindo do sistema...")
+    }
+
+    else {
+        console.log("Opção Invalida!")
+    }
+}
+}
+
+MostraMenu();
+
+// Quando todas a funções do menu estiverem criadas irei alterar meu código (Lucas)
+
+
+  function sleep(milisegundos) {
+    return new Promise((ok, nok) => {
+      setTimeout(ok, milisegundos);
+    })
+}
--- function.js
+++ function.js
@@ -1,101 +1,142 @@
 import prompt from 'prompt-sync'
 let ler = prompt();
 
+let estoque = [
+  {
+    nome: 'The Last of Us Part II',
+    categoria: 'jogo',
+    plataforma: 'PS5',
+    preco: 249.90,
+    quantidade: 10
+  },
+  {
+    nome: 'Xbox Series X',
+    categoria: 'console',
+    plataforma: 'Xbox',
+    preco: 3999.00,
+    quantidade: 5
+  },
+  {
+    nome: 'Controle DualSense',
+    categoria: 'acessório',
+    plataforma: 'PS5',
+    preco: 399.90,
+    quantidade: 15
+  },
+  {
+    nome: 'Nintendo Switch OLED',
+    categoria: 'console',
+    plataforma: 'Nintendo Switch',
+    preco: 2999.00,
+    quantidade: 8
+  },
+  {
+    nome: 'Cyberpunk 2077',
+    categoria: 'jogo',
+    plataforma: 'PC',
+    preco: 199.90,
+    quantidade: 20
+  },
+  {
+    nome: 'Headset Gamer HyperX',
+    categoria: 'acessório',
+    plataforma: 'Multi',
+    preco: 350.00,
+    quantidade: 12
+  },
+
+  {
+    nome: 'Impressora 3D',
+    categoria: 'Utilidades',
+    plataforma: 'Informatica',
+    preco: 8050.00,
+    quantidade: 90
+  }
+];
+
+
+export function Cadastrarproduto() {
+    console.log('Nome do produto: 📘')
+    let nome = ler();
+
+    console.log('Categoria: (Jogo, console, acessorio): 💻')
+    let categoria = ler();
+
+    console.log('Plataforma (PS5, Xbox, PC, Nintendo Switch): 🖥️');
+    let plataforma = ler();
+
+    console.log('Preço: R$ ');
+    let preco = Number(ler());
+
+    console.log('Digite a quantidade inicial: 💵')
+    let quantidade = Number(ler());
+
+    let produto = {
+        nome,
+        categoria,
+        plataforma,
+        preco,
+        quantidade,
+    };
+
+    estoque.push(produto);
+    console.log('✅ Produto cadastrado com sucesso!')
+}
 
 
 
+export async function listarprodutos() {
+  if (estoque.length === 0) {
+    console.log('🛒 O estoque está vazio!');
+    return;
+  }
+
+  console.log('------ LISTA DE PRODUTOS --------');
+
+  for (let i = 0; i < estoque.length; i++) {
+    let produto = estoque[i];
+    console.log(`\nProduto ${i + 1}:`);
+
+    console.log(`Nome: ${produto.nome}`);
+
+    console.log(`Categoria: ${produto.categoria}`);
+
+    console.log(`Plataforma: ${produto.plataforma}`);
+
+    console.log(`Preço: R$ ${produto.preco}`);
+
+    console.log(`Quantidade em estoque: ${produto.quantidade}`);
+  }
+}
+
+// estoque baixo riquelme
 
 
 
+export function adicionarEstoque() {
+    listarprodutos();
 
-//Função de Apresentar menu Lucas Gabriel
+    console.log('Numero do produto para adicionar estoque: ')
+    let posicao = Number(ler()) -1;
 
-export async function MostraMenu () {
+    if(posicao >= 0 && posicao < estoque.length){
 
-    let funcao = 1;
+        console.log('Digite a quantidade a se adicionar: ');
+        let qtd = Number(ler()) 
+        estoque[posicao].quantidade += qtd;
 
-    while (funcao !== 0) {
+        console.log('✅ Estoque atualizado com sucesso!');
 
-    
-
-    console.log("=== SISTEMA DE ESTOQUE 🎮 ===")
-    
-    await sleep(200)
-    console.log("0. Sair")
-    await sleep(200)
-    console.log("1. Cadastrar novo produto")
-    await sleep(200)
-    console.log("2. Listar todos os produtos")
-    await sleep(200)
-    console.log("3. Adicionar Quantidade ao estoque")
-    await sleep(200)
-    console.log("4. Retirar quantidade do estoque")
-    await sleep(200)
-    console.log("5. Apagar produto")
-    await sleep(200)
-    console.log("6. Calcular valor do Estoque")
-    await sleep(200)
-    console.log("7. Buscar produto pelo nome")
-    
-    await sleep(200)
-
-    console.log("Qual função você irá usar?")
-    funcao = Number(ler())
-    
-    if (funcao == 1) {
-        //função não foi criada ainda
-    }
-
-    else if (funcao == 2) {
-        //função não foi criada ainda
-    }
-
-    else if (funcao == 3) {
-        //função não foi criada ainda
-    }
-
-    else if (funcao == 4) {
-        //função não foi criada ainda
-    }
-
-    else if (funcao == 5) {
-       apagarProduto();
-    }
-
-    else if (funcao == 6) {
-        calcularValorEstoque();
-    }
-
-    else if (funcao == 7) {
-
-    }
-
-    else if (funcao == 0) {
-        console.log("Saindo do sistema...")
     }
 
     else {
-        console.log("Opção Invalida!")
+        console.log('Produto invalido!')
     }
 }
-}
-
-// Quando todas a funções do menu estiverem criadas irei alterar meu código (Lucas)
-
-
-  function sleep(milisegundos) {
-    return new Promise((ok, nok) => {
-      setTimeout(ok, milisegundos);
-    })
-}
 
 
 
-
-
-
-
-export function apagarProduto(){
+export function Retirardoestoque(){
     console.log('Numero do produto para retirar do estoque')
     let pos = Number(ler()) - 1;
     
@@ -113,28 +154,86 @@ export function apagarProduto(){
     }
 }
 
-
-//função de somar o total do estoque -- Ana Clara 
- export function calcularValorEstoque (){ 
-    let valorTotal= 0; //Valor total começa com 0 
-     
-    for (let produtos of produtos){
-        valorTotal + produco.preco * produto.quantidade; //Calcular o valor total dos produtos e adiciona á valorTotal
+export async function  buscarProduto() {
+    listarprodutos();
+    
+    console.log('Digite a Plataforma do produto: ');
+    let plataformaa = ler();
+    
+    for(let i = 0; i < estoque.length; i++) {
+        if(estoque[i].plataforma.toLowerCase() == plataformaa.toLowerCase()) {
+            console.log(`🔎 Produto encontrado:`, estoque[i])
+        }
     }
+}
 
-    console.log(`O valor total do estoque é: ${valorTotal}`)//Apresenta o valor total do estoque ao usuário
- }
+export function apagarProduto() {
+    listarprodutos();
 
+    console.log('Numero produto a ser apagado: ');
+    let posicao = Number(ler()) -1;
 
-  export function buscarprodutopelonome (){
-    console.log('digita o nome do produto que voce deseja buscar')
-    let nomeproduto = ler ();
-
-    if (nomeproduto === produto [produtos].nome) {
-        console.log(`o nome do produto é ${nome}` )
+    if(posicao >= 0 && posicao < estoque.length) {
+        estoque.splice(posicao, 1);
+        console.log('✅ Produtos atualizado com sucesso!');
     }
 
     else {
-        ('nenhum produto encotrado')
+        console.log('Produto invalido!')
     }
-  }
+}
+
+    export function atualizarProduto() {
+    listarprodutos();
+
+    console.log('\nDigite o número do produto que deseja atualizar:');
+    let posicao = Number(ler()) - 1;
+
+    if (posicao >= 0 && posicao < estoque.length) {
+        console.log('\nO que você deseja atualizar?');
+        console.log('1 - Nome');
+        console.log('2 - Preço');
+    let opcao = Number(ler());
+
+    if (opcao === 1) {
+        console.log('Digite o novo nome do produto:');
+        let novoNome = ler();
+        estoque[posicao].nome = novoNome;
+        console.log('✅ Nome atualizado com sucesso!');
+    } 
+
+    else if (opcao === 2) {
+        console.log('Digite o novo preço do produto:');
+        let novoPreco = Number(ler());
+        estoque[posicao].preco = novoPreco;
+        console.log('✅ Preço atualizado com sucesso!');
+    } 
+
+    else {
+        console.log('❌ Opção inválida!');
+    }
+
+    } 
+
+    else {
+        console.log('❌ Produto inválido!');
+    }
+}
+
+
+//função de somar o total do estoque -- Ana Clara 
+    export function calcularValorEstoque (){ 
+    let valorTotal= 0; //Valor total começa com 0 
+        
+    for (let produtos of estoque){
+        valorTotal += produtos.preco * produtos.quantidade; //Calcular o valor total dos produtos e adiciona á valorTotal
+    }
+
+    console.log(`O valor total do estoque é: ${valorTotal}`)//Apresenta o valor total do estoque ao usuário
+}
+
+function sleep(mili) {
+  return new Promise((ok) => {
+    setTimeout(ok, mili);
+  })
+}
